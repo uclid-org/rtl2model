35d17c009d1f2c5e56f3e7c2b728f634e09ed5f3
noloerino <jhshi07@gmail.com>
2022-04-21 11:01:31 -0700

    push missing files

---
+++ examples/r8051_rtl_first.py
@@ -0,0 +1,166 @@
+import os
+import subprocess
+
+from easyila.guidance import Guidance, AnnoType
+from easyila.synthesis_template import *
+from easyila.sketch import *
+import easyila.gen_config as gen_config
+import easyila.lynth.smt as smt
+from easyila.verilog import *
+
+# Things needed:
+# - verilator file + Makefile + csv emitting stuff (tracing_manager)
+# - possibly instrumented variables
+
+# TODO
+REPO_BASE_DIR = "/home/jhshi/research/hwlifting/"
+BASEDIR = os.path.join(REPO_BASE_DIR, "processors/R8051/")
+
+class R8051Model(ModelBuilder):
+    def build_binary(self):
+        gen_config.generate_config(
+            signals=self.signals,
+            target_sim_dir=BASEDIR,
+            target_verilog_dir=BASEDIR,
+            root="top"
+        )
+        self.run_proc(["make", "default"], cwd=BASEDIR)
+
+    def generate_program(self, inputs) -> ConcreteProgram:
+        # https://www.keil.com/support/man/docs/is51/is51_opcodes.htm
+        return ProgramSketch(
+            inst_byte(0x00),       # nop
+            inst_byte(0x78),       # MOV R0, imm
+            Inst(SketchHole("i0", 8)),         # (imm)
+            inst_byte(0x74),       # MOV A, imm
+            Inst(SketchHole("i1", 8)),         # (imm)
+            inst_byte(0x28),       # ADD A, R0
+            inst_byte(0x00),       # NOP
+            inst_byte(0x00),
+            inst_byte(0x00),
+            inst_byte(0x31),       # ACALL addr11
+            inst_byte(0x31),
+            inst_byte(0x31),
+            inst_byte(0x31),
+            inst_byte(0x31),
+            inst_byte(0x31),
+            inst_byte(0x31)
+        ).fill({"i0": inputs[0], "i1": inputs[1]})
+
+    def simulate_and_read_signals(self, program):
+        with open(os.path.join(BASEDIR, "myhello"), "wb") as f:
+            f.write(program.to_bytearray())
+        self.run_proc(["./obj_dir/Vtop"], cwd=BASEDIR)
+        return read_csv(os.path.join(BASEDIR, "traces/tr.csv"), self.cycle_count)
+
+def main():
+    core = verilog_to_model(
+        os.path.join(BASEDIR, "full.v"),
+        "r8051",
+        clock_pattern="clk",
+        important_signals=[
+            "rst",
+            "lft_acc",
+            "lft_psw_c",
+            "lft_cmd0",
+            "lft_cmd1",
+            "lft_cmd2",
+            "ram_wr_byte",
+            "lft_data1",
+        ],
+        coi_conf=COIConf.UF_ARGS_COI,
+    )
+    core = core.eliminate_dead_code()
+    core.print()
+    core.validate()
+    print(core.to_solver().get_sygus2())
+
+    return
+
+
+    cycle_count = 10
+    # TODO allow hierarchical specification
+    """
+    SIGNALS = {
+        "tb": {
+            "clk": 1,
+            "rst": 1,
+            etc.
+        }
+    }
+    """
+    pc_sig = S("tb", "lft_pc", 16)
+    SIGNALS = [
+        S("tb", "clk", 1),
+        S("tb", "rst", 1),
+        pc_sig,
+        S("tb", "lft_acc", 8),
+        S("tb", "lft_psw_c", 1),
+        S("tb", "lft_cmd0", 8),
+        S("tb", "lft_cmd1", 8),
+        S("tb", "lft_cmd2", 8),
+        S("tb", "ram_wr_byte", 8),
+        S("tb", "data", 8, bounds=(0, 7)),
+    ]
+    guidance = Guidance(SIGNALS, cycle_count)
+    for sig in ("rst", "lft_pc"):
+        guidance.annotate(sig, AnnoType.ASSUME)
+    guidance.annotate("lft_cmd0", {
+        pc_sig.to_variable().op_eq(3): AnnoType.Param,
+        pc_sig.to_variable().op_eq(5): AnnoType.Param,
+        smt.BoolConst.T: AnnoType.ASSUME,
+    })
+    guidance.annotate("lft_acc", {
+        pc_sig.to_variable().op_eq(8): AnnoType.OUTPUT,
+        smt.BoolConst.T: AnnoType.ASSUME,
+    })
+
+    bv8 = smt.BVSort(8)
+    # TODO create refinement mappings for smt variables to verilog names
+    # or automatically map shadow vars to inputs
+    # x = smt.Variable("arg_0", bv8)
+    # y = smt.Variable("arg_1", bv8)
+    x = smt.Variable("__shadow_0", bv8)
+    y = smt.Variable("__shadow_1", bv8)
+    start = smt.Variable("Start", bv8)
+    substart = smt.Variable("BV8", bv8)
+    boolterm = smt.Variable("B", smt.BoolSort())
+    addbv = start + start
+    subbv = start - start
+    orbv = start | start
+    andbool = boolterm & boolterm
+    orbool = boolterm | boolterm
+    xorbool = boolterm ^ boolterm
+    notbool = ~boolterm
+    impliesbool = boolterm.implies(boolterm)
+    itebv = boolterm.ite(substart, substart)
+    # Synth function and grammar
+    solver = smt.SynthFun(
+        "alu_add",
+        (x, y),
+        bv8,
+        smt.Grammar(
+            (x, y),
+            (start, boolterm, substart),
+            # Order of these terms matters
+            # Python dicts preserve insertion/declaration order
+            {
+                start: (addbv, subbv, orbv, itebv),
+                boolterm: (andbool, orbool, notbool, impliesbool, xorbool, smt.BoolConst.T),
+                substart: (x, y),
+            }
+        )
+    ).new_solver()
+
+    model = R8051Model(
+        ProjectConfig(os.path.join(BASEDIR, "symbiyosys")),
+        solver,
+        SIGNALS,
+        guidance
+    )
+    model.build_binary()
+    model.main_sygus_loop()
+
+if __name__ == "__main__":
+    main()
+
---
+++ tests/model_generation/test_vcd_wrapper.py
@@ -0,0 +1,110 @@
+"""
+Tests wrapper for reading from a VCD file.
+"""
+
+from easyila.verilog import VcdWrapper
+
+vcdtext = """\
+$date 2022-02-14 15:54:18.657260 $end
+$timescale 1 ns $end
+$scope module Tile $end
+$var wire 1 ! clk $end
+$var wire 1 " r $end
+$var reg 4 # ctr $end
+$var reg 2 $ s $end
+$var wire 2 % t $end
+$upscope $end
+$enddefinitions $end
+#0
+$dumpvars
+1!
+0"
+b0 #
+b0 $
+b0 %
+$end
+b1 $
+b11 %
+1"
+#1
+0!
+#2
+1!
+b10 $
+b0 %
+0"
+b1 #
+#3
+0!
+#4
+1!
+b11 $
+b1 %
+b10 #
+#5
+0!
+#6
+1!
+b0 $
+b10 %
+1"
+b11 #
+#7
+0!
+#8
+1!
+b1 $
+b11 %
+b100 #
+#9
+0!
+#10
+1!
+b10 $
+b0 %
+0"
+b101 #
+#11
+0!
+#12
+1!
+b11 $
+b1 %
+b110 #
+#13
+0!
+#14
+1!
+b0 $
+b10 %
+1"
+b111 #
+#15
+0!
+#16
+1!
+b1 $
+b11 %
+b1000 #
+#17
+0!
+#18
+1!
+b10 $
+b0 %
+0"
+b1001 #
+#19
+0!
+#20
+1!
+"""
+
+class TestVcdWrapper:
+    def test_vcd_read(self, tmp_path):
+        tmp_file = tmp_path / "test.vcd"
+        with open(tmp_file, "w") as f:
+            f.write(vcdtext)
+        r = VcdWrapper(tmp_file, "Tile.clk")
+        assert r.get_value_at("Tile.ctr", 0) == 0
+        assert r.get_value_at("Tile.ctr", 1) == 1
